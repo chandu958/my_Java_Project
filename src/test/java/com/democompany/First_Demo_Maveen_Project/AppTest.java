242809a8f07a6cc47e7785b2b9314ab545d62937
vamsi <lovelyramsahu1234@gmail.com>
2023-09-09 13:36:25 +0530

    my first java project pushing to remote

---
+++ src/test/java/com/democompany/First_Demo_Maveen_Project/AppTest.java
@@ -0,0 +1,20 @@
+package com.democompany.First_Demo_Maveen_Project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
